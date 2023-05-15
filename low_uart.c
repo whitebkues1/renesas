80bf80b9396729f67850b02cd0721bfada6eb7c0
whitebkues1 <58509780+whitebkues1@users.noreply.github.com>
2023-05-15 16:25:32 +0800

    Update low_uart.c

--- low_uart.c
+++ low_uart.c
@@ -7,3 +7,23 @@ int __io_putchar(int ch);
 int __io_getchar(void);
 int _write(int fd,char *pBuffer,int size);
 int _read(int fd, char *pBuffer, int size);
+
+void user_uart_callback (uart_callback_args_t * p_args)
+{
+    if(p_args->event == UART_EVENT_TX_COMPLETE)
+    {
+        uart_send_complete_flag = true;
+    }
+
+    if(UART_EVENT_RX_COMPLETE == p_args->event)
+    {
+        uart_receive_complete_flag = true;
+    }
+}
+
+#ifdef __GNUC__
+    #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+    #define GETCHAR_PROTOTYPE int __io_getchar(void)
+#else
+    #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
